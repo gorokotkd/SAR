c44d127688397c7fb5b1392e4cfd4d25d2a74dc2
Gorka <galvarez024@ikasle.ehu.eus>
2019-11-13 20:15:45 +0100

    Iniciado Proyecto

---
+++ PracticaGeneral3/php/Layout.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <?php include'../html/Head.html'?>
+    </head>
+    <body>
+        <?php include'../php/Menus.php'?>
+    </body>
+</html>
---
+++ PracticaGeneral3/php/Menus.php
@@ -0,0 +1,6 @@
+<div class="nav-bar">
+    <a class="active" href="#home">Inicio</a>
+    <a href="#images">Ver Imagenes</a>
+    <a href="#coment-image">Comentar Imagen</a>
+    <a href="#about">Creditos</a>
+</div>
