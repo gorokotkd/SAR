e8ebc42a337541e50b5ee572cf1f16bd5ae3a498
Gorka <galvarez024@ikasle.ehu.eus>
2019-11-20 22:39:11 +0100

    Ya se añaden imagenes

--- PracticaGeneral3/php/AddImage.php
+++ PracticaGeneral3/php/AddImage.php
@@ -7,23 +7,25 @@
         <?php include'../php/Menus.php'?>
         <section id="s1" class="main">
             <div>
-                <form id="f-add-image" method="get" enctype="multipart/form-data">
+                <form id="f-add-image" method="post" enctype="multipart/form-data">
                     <div>
                         <label>Introduce tu correo electronico: *</label><br>
                         <input type="email" name="email" size="50" required pattern="((^[a-zA-Z]+(([0-9]{3})+@ikasle\.ehu\.(eus|es))$)|^[a-zA-Z]+(\.[a-zA-Z]+@ehu\.(eus|es)|@ehu\.(eus|es))$)">
                     </div>
                     <div>
                         <label>Selecciona la categoria de la imagen: *</label><br>
-                        <input type="radio" value="paisaje" name="categoria">Paisajes
-                        <input type="radio" value="ciudad" name="categoria">Ciudades<br>
-                        <input type="radio" value="deporte" name="categoria">Deportes
-                        <input type="radio" value="animal" name="categoria">Animales<br>
-                        <input type="radio" value="comida" name="categoria">Comida
-                        <input type="radio" value="otra" name="categoria" checked><b>Otras</b>
+                        <select id="categoria" name="categoria">
+                            <option value="paisaje">Paisajes</option><
+                            option value="ciudad">Ciudades</option>
+                            <option value="deporte">Deportes</option>
+                            <option value="animal">Animales</option>
+                            <option value="comida">Comida</option>
+                            <option value="otra" selected>Otras</option>
+                        </select>
                     </div>
                     <div>
                         <label>Selecciona la imagen a subir: *</label><br>
-                        <input type="file" id="file" accept="image/*" name="Imagen" onchange="verImagen(this)" required>
+                        <input type="file" id="file" accept="image/*" name="file" onchange="verImagen(this)" required>
                     </div>
                     <div id="img-muestra"></div>
                     <div>
@@ -36,15 +38,31 @@
         </section>
         
         <?php
+        /*
+        EN ESTA PARTE QUEDARIA AÑADIR LA IMAGEN AÑADIDA AL XML DE USUARIOS
+        */
             if(isset($_REQUEST['email'])){
-                phpinfo();
-                $imagePath = "../images/";
-                $imagename = $_FILES['Imagen']['name'];
-                $imagetemp = $_FILES['Imagen']['tmp_name'];
+                $xml = simplexml_load_file('../xml/images.xml');
+                $xml['ult_id']  += 1;
+                $imagen = $xml->addChild('imagen');
+                $imagen->addAttribute('id',$xml['ult_id']);
                 
-                if(move_uploaded_file($imagetemp,$imagePath.$imagename)){
-                    echo "Yupi";
+                $datos = $imagen->addChild('datos');
+                $datos->addChild('propietario',$_REQUEST['email']);
+                $datos->addChild('fecha',date("d.m.y"));
+                $datos->addChild('categoria',$_REQUEST['categoria']);
+                
+                
+                $imagePath = "../images/";
+                $imagename = "img_".$xml['ult_id'].".jpg";
+                $imagetemp = $_FILES['file']['tmp_name'];
+                
+                if(!move_uploaded_file($imagetemp,$imagePath.$imagename)){
+                    echo"Ha ocurrido un error al mover el archivo.";
                 }
+                
+                $imagen->addChild('path',$imagename);
+                $xml->asXML('../xml/images.xml');
             }
         
         ?>
